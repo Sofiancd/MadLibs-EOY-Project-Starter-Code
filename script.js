5c8a44879e747becc1e358af7b19b86dcfdf4307
Sofiancd <104465823+Sofiancd@users.noreply.github.com>
2022-05-10 14:21:23 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let storiesCreated;
+let counter=0;
 $("button").click(function() {
     let name = $(".persons-name").val();
     console.log(name);
-    let transport = $(".type-of-transport").val();
-    console.log(transport);
+    let transportation = $(".type-of-transport").val();
+    console.log(transportation);
     let adjective = $(".adjective").val();
     console.log(adjective);
-    let time = $(".number").val();
-    console.log(time);
-    $(".story").append("One day my friend "+name+" and I decided to take a trip to the beach, so we packed our bags and took the "+transport+"over.It was a"+adjective+"day at the beach! We ended up staying for"+time+"hours!");
+    let number = $(".number").val();
+    console.log(number);
+    $(".story").append("One day my friend "+name+" and I decided to take a trip to the beach, so we packed our bags and took the "+transportation+"over.It was a "+adjective+" day at the beach! We ended up staying for "+number+" hours!");
+    counter= counter + 1;
+     $(".counter-display").text("Counter: "+ counter);
 });
